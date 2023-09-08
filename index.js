b0c365fb146d4e612aa2b9e0c9e3c6383dd7356f
Ikram Akbar <ikramakbar71.com>
2023-09-08 11:47:41 +0600

    mogodb

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require("dotenv").config()
+const port = process.env.PORT || 5000;
+
+//middleware: 
+app.use(cors())
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f8emp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
-  res.send("<h1>Coffee shop is ON </h1>")
+    res.send("<h1>Coffee shop is ON </h1>")
 })
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
